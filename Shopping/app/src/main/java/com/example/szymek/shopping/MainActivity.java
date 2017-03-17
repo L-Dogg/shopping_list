94aef17deb9dac0704a096ccfe7ee03386bfa0f2
L-Dogg <szymek94sul@gmail.com>
2017-03-17 01:11:29 +0100

    Add icon. Add back arrow in action bar.

--- Shopping/app/src/main/java/com/example/szymek/shopping/MainActivity.java
+++ Shopping/app/src/main/java/com/example/szymek/shopping/MainActivity.java
@@ -17,18 +17,27 @@ import android.widget.Toast;
  * Created by Szymek on 10-Mar-17.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //setTheme(android.R.style.Text);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FrameLayout viewById = (FrameLayout) findViewById(R.id.main_container);
 
-        getFragmentManager().beginTransaction()
+        if (savedInstanceState == null)
+            getFragmentManager().beginTransaction()
                 .replace(R.id.main_container, new MainFragment())
-                //.addToBackStack(null)
                 .commit();
+
+        //Listen for changes in the back stack
+        getFragmentManager().addOnBackStackChangedListener(new android.app.FragmentManager.OnBackStackChangedListener() {
+            @Override
+            public void onBackStackChanged() {
+                shouldDisplayHomeUp();
+            }
+        });
+        //Handle when activity is recreated like on orientation Change
+        shouldDisplayHomeUp();
     }
 
     @Override
@@ -39,15 +48,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         getFragmentManager().beginTransaction()
                 .replace(R.id.main_container, new SettingsFragment())
-                .addToBackStack(null)
+                .addToBackStack("settingsFragment")
                 .commit();
+
+        shouldDisplayHomeUp();
+
         return super.onOptionsItemSelected(item);
+    }
+
+    public void shouldDisplayHomeUp(){
+        //Enable Up button only  if there are entries in the back stack
+        boolean canback = getFragmentManager().getBackStackEntryCount()>0;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(canback);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        //This method is called when the up button is pressed. Just the pop back stack.
+        getFragmentManager().popBackStack("settingsFragment", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        return true;
     }
 }
